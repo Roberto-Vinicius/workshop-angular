e97813fb33529cb00ec1a23b564740cbf5f00766
roberto <roberto.dantas.706@ufrn.edu.br>
2024-08-15 12:31:22 -0300

    Atualização de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { DirectivesComponent } from "./components/directives/directives.component";
-import { EmitterComponent } from "./components/emitter/emitter.component";
-import { EventosComponent } from "./components/eventos/eventos.component";
-import { FirstComponentComponent } from "./components/first-component/first-component.component";
-import { IfRenderComponent } from "./components/if-render/if-render.component";
-import { ParentDataComponent } from "./components/parent-data/parent-data.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, DirectivesComponent, EmitterComponent, EventosComponent, FirstComponentComponent, IfRenderComponent, ParentDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'workshop-angular';
+
+  userName = 'JAO';
+  userData = {
+    email: 'roberto@123',
+    role: 'admin',
+  }
+
+  title = 'workshop-front';
 }
